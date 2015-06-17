46348ed2f55f90a8d87f26ee551bf77d4a752023
ending06 <040137@163.com>
2015-06-17 10:40:43 +0800

    start spring study

---
+++ springprac-parent/springenterprise/src/main/java/com/springEnterprise/ruirui/qu/aop/test.java
@@ -0,0 +1,8 @@
+package com.springEnterprise.ruirui.qu.aop;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2015/6/17<p>
+//-------------------------------------------------------
+
+public class test {
+}
