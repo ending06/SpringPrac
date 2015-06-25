e0a23f9da19ba4362b1539daa1a2df1944c25bb8
ending06 <040137@163.com>
2015-06-25 19:40:11 +0800

    add chapter3 collection injection

---
+++ springprac-parent/spring-techmanual/src/main/java/com/techmanual/chapterthree/collection/School.java
@@ -0,0 +1,8 @@
+package com.techmanual.chapterthree.collection;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2015/6/25<p>
+//-------------------------------------------------------
+
+public class School {
+}
