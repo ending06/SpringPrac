95b663e6e6bb53648786874dc532dff835486fdc
ending06 <040137@163.com>
2015-06-29 11:33:24 +0800

    add message

---
+++ springprac-parent/spring-techmanual/src/main/java/com/techmanual/chapterthree/publishevent/SpringDemo.java
@@ -0,0 +1,8 @@
+package com.techmanual.chapterthree.publishevent;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2015/6/29<p>
+//-------------------------------------------------------
+
+public class SpringDemo {
+}
