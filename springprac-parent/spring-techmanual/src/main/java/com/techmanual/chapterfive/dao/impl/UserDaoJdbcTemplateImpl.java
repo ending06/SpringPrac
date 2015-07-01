17604f2fbfc3d7e0ba3ee153c94bb57781050f6f
ending06 <040137@163.com>
2015-07-01 14:40:04 +0800

    jdbcTemplate

---
+++ springprac-parent/spring-techmanual/src/main/java/com/techmanual/chapterfive/dao/impl/UserDaoJdbcTemplateImpl.java
@@ -0,0 +1,8 @@
+package com.techmanual.chapterfive.dao.impl;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2015/7/1<p>
+//-------------------------------------------------------
+
+public class UserDaoJdbcTemplateImpl {
+}
