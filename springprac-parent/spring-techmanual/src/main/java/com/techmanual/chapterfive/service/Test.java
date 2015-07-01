133e4cd54435d0f921b11bb8961dcf8473318c00
ending06 <040137@163.com>
2015-07-01 20:04:47 +0800

    del  hibernate

--- springprac-parent/spring-techmanual/src/main/java/com/techmanual/chapterfive/service/Test.java
+++
@@ -1,8 +0,0 @@
-package com.techmanual.chapterfive.service;
-
-//--------------------- Change Logs----------------------
-// <p>@author ruirui.qu Initial Created at 2015/6/30<p>
-//-------------------------------------------------------
-
-public class Test {
-}
--- springprac-parent/spring-techmanual/src/main/java/com/techmanual/chapterfive/service/impl/Test.java
+++
@@ -1,8 +0,0 @@
-package com.techmanual.chapterfive.service.impl;
-
-//--------------------- Change Logs----------------------
-// <p>@author ruirui.qu Initial Created at 2015/6/30<p>
-//-------------------------------------------------------
-
-public class Test {
-}
