15067750ab8b13e8fd7bf0d425660d88d866c24e
ending06 <040137@163.com>
2015-07-31 17:51:31 +0800

    java-practice

---
+++ springprac-parent/java-practice/src/main/java/com/java/practice/generics/Generics.java
@@ -0,0 +1,9 @@
+package com.java.practice.generics;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2015/7/31<p>
+// 泛型
+// -------------------------------------------------------
+
+public class Generics {
+}
